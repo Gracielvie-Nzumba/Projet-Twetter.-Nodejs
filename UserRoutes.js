25808f411379d98446c6a911b9ce3d3e15ca61b2
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-02-09 11:33:09 +0200

    serveur2

--- UserRoutes.js
+++ UserRoutes.js
@@ -9,28 +9,3 @@ router.put('/tweets/:id', tweetsController.updateTweet);
 router.delete('/tweets/:id', tweetsController.deleteTweet);
 
 module.exports = router;
-
-
-
-// const express = require('express');
-// const router = express.Router();
-// const controllers = require('../controllers/controllers.js');
-// const { getAll } = require('./TweetsControllers.js');
-
-// router.get("", getAll)
-
-// router.get('/:id',function (req,res){
-//     res.send("Bienvenu sur notre page" + req.params.id)
-
-// });
-
-// router.post('', controllers.create);
-
-// router.put('/:id', controllers(req,res));
-// res.send("Page d'accueil")
-
-// router.delete('/:id',function(req,res){
-//     res.send("Message Supprimé");
-// });
-
-// module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// const routes = require('./routes');
 
-// app.use('/', routes);
 app.get('/', (req, res) => {
   res.send('Bienvenue sur notre Site !');
 });
@@ -11,25 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('Serveur en écoute sur le port 3000');
 });
-// console.log("hui");
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const routes = require('./routes');
-// const port = 3000;
 
-// const app = express();
-
-// // mongoose.connect('mongodb://localhost:27017/database', {
-// //   useNewUrlParser: true,
-// //   useUnifiedTopology: true
-// // });
-
-// app.set()
-// app.set()
-// app.use(express.urlencoded({extended:true}));
-// app.use('/Users',Users)
-// app.use('/Model', Model);
-
-// app.listen(3000, () => {
-//   console.log("Serveur en cours d'exécution sur le port 3000");
-// });
