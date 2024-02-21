6ece271a695a54753ed61a390629094a3f144f8e
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-02-21 16:59:50 +0200

    mise a j

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { authentificate } = require('passport');
+const { authenticate } = require('passport');
 const port = 3000;
 const app = express();
 
